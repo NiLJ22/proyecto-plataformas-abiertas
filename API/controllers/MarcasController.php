6aa0af2f1219bb30251e89bac95858b74cea6508
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:32:51 -0600

    Update MarcasController.php

--- API/controllers/MarcasController.php
+++ API/controllers/MarcasController.php
@@ -1,60 +1,31 @@
 <?php
-require 'models/Marca.php';
+require_once __DIR__ . '/../models/Marca.php';
 
-class MarcasController {
-    private $marca;
+class MarcaController {
+    private $marcaModel;
 
     public function __construct() {
-        $this->marca = new Marca();
+        $this->marcaModel = new Marca();
     }
 
-    public function handleRequest($method, $uri, $input) {
-        switch ($method) {
-            case 'GET':
-                if (isset($uri[3])) {
-                    $this->getMarca($uri[3]);
-                } else {
-                    $this->getMarcas();
-                }
-                break;
-            case 'POST':
-                $this->createMarca($input);
-                break;
-            case 'PUT':
-                $this->updateMarca($uri[3], $input);
-                break;
-            case 'DELETE':
-                $this->deleteMarca($uri[3]);
-                break;
-            default:
-                http_response_code(405);
-                echo json_encode(['message' => 'Method not allowed']);
-                break;
+    // Example method to get Marca details by ID
+    public function getMarcaById($id) {
+        $marca = $this->marcaModel->getById($id);
+        if ($marca) {
+            // Return marca details in JSON format
+            $response = array(
+                'id' => $marca->getId(),
+                'nombre' => $marca->getNombre(),
+                'descripcion' => $marca->getDescripcion(),
+                'estilo' => $marca->getEstilo()
+            );
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode($response);
+        } else {
+            // Marca not found
+            http_response_code(404); // Not Found
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode(array('message' => 'Marca not found'));
         }
-    }
-
-    private function getMarcas() {
-        $result = $this->marca->read();
-        echo json_encode($result);
-    }
-
-    private function getMarca($id) {
-        $result = $this->marca->readOne($id);
-        echo json_encode($result);
-    }
-
-    private function createMarca($input) {
-        $this->marca->create($input);
-        echo json_encode(['message' => 'Marca creada']);
-    }
-
-    private function updateMarca($id, $input) {
-        $this->marca->update($id, $input);
-        echo json_encode(['message' => 'Marca actualizada']);
-    }
-
-    private function deleteMarca($id) {
-        $this->marca->delete($id);
-        echo json_encode(['message' => 'Marca eliminada']);
     }
 }
