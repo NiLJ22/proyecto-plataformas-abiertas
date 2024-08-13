5c9df7193a7f31319c1f55f5261a21e5e149a6e1
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:33:03 -0600

    Update api.php

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,4 +1,20 @@
 <?php
-require 'controllers/MarcasController.php';
-require 'controllers/PrendasController.php';
-require 'controllers/VentasController.php';
+require_once "controllers/MarcasController.php";
+
+$marcaController = new MarcaController();
+
+// Handle GET request for fetching user details by ID
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $response = $marcaController->getMarcaById($id);
+        echo $response;
+    } else {
+        http_response_code(400); // Bad request
+        echo json_encode(array('message' => 'ID parameter is required'));
+    }
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(array('message' => 'Method not allowed'));
+}
+?>
