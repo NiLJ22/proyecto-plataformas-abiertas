6a5071cac46d63017a04a9edb36448589a650e34
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:58:59 -0600

    Update api.php

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,20 +1,47 @@
 <?php
-require_once "controllers/MarcasController.php";
+require_once __DIR__ . '/../controllers/MarcaController.php';
+require_once __DIR__ . '/../controllers/PrendasController.php';
+require_once __DIR__ . '/../controllers/VentasController.php';
 
+// Initialize controllers
 $marcaController = new MarcaController();
+$prendasController = new PrendasController();
+$ventasController = new VentasController();
 
-// Handle GET request for fetching user details by ID
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['id'])) {
-        $id = $_GET['id'];
-        $response = $marcaController->getMarcaById($id);
-        echo $response;
-    } else {
-        http_response_code(400); // Bad request
-        echo json_encode(array('message' => 'ID parameter is required'));
-    }
-} else {
-    http_response_code(405); // Method Not Allowed
-    echo json_encode(array('message' => 'Method not allowed'));
+// Routing based on HTTP method and URI
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = explode('/', $_SERVER['REQUEST_URI']);
+
+switch ($uri[1]) {
+    case 'marcas':
+        if ($method == 'GET' && isset($uri[2])) {
+            echo $marcaController->getMarcaById($uri[2]);
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Marca endpoint not found']);
+        }
+        break;
+
+    case 'prendas':
+        if ($method == 'GET' && isset($uri[2])) {
+            echo $prendasController->getPrendaById($uri[2]);
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Prenda endpoint not found']);
+        }
+        break;
+
+    case 'ventas':
+        if ($method == 'GET' && isset($uri[2])) {
+            echo $ventasController->getVentaById($uri[2]);
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Venta endpoint not found']);
+        }
+        break;
+
+    default:
+        http_response_code(404);
+        echo json_encode(['message' => 'Endpoint not found']);
 }
 ?>
