e9c637d9f59517c89b4b30c9ae6090fbf9335d95
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:32:57 -0600

    Update index.php

--- API/index.php
+++ API/index.php
@@ -1,27 +1,26 @@
 <?php
-require 'database/db.php';
-require 'routes/api.php';
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
-// Configuración de encabezados para permitir solicitudes desde cualquier origen
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+// Routing
+$requestUri = $_SERVER['REQUEST_URI'];
+$basePath = '/proyecto-api/API/index.php'; // Adjust this if your API is located in a subdirectory
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode('/', $uri);
-
-$controller = $uri[2] . 'Controller';
-$method = $_SERVER['REQUEST_METHOD'];
-
-$input = json_decode(file_get_contents('php://input'), true);
-
-// Llama a la función adecuada del controlador
-if (file_exists("controllers/{$controller}.php")) {
-    require "controllers/{$controller}.php";
-    $controllerInstance = new $controller();
-    $controllerInstance->handleRequest($method, $uri, $input);
-} else {
-    http_response_code(404);
-    echo json_encode(['message' => 'Endpoint not found']);
+// Remove base path from request URI
+if (strpos($requestUri, $basePath) === 0) {
+    $requestUri = substr($requestUri, strlen($basePath));
 }
+
+// Remove query string from request URI
+$requestUri = strtok($requestUri, '?');
+
+// Route requests
+switch ($requestUri) {
+    case '/marcas':
+        require_once   './routes/api.php';
+        break;
+    default:
+        http_response_code(404); // Not Found
+        echo json_encode(['message' => 'Endpoint not found']);
+}
+?>
