3545a3fc58c1037d9ba1ab757f2686cdc5a0fe0f
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:58:47 -0600

    Update PrendasController.php

--- API/controllers/PrendasController.php
+++ API/controllers/PrendasController.php
@@ -1,60 +1,31 @@
 <?php
-require 'models/Prenda.php';
+require_once __DIR__ . '/../models/Prenda.php';
 
 class PrendasController {
-    private $prenda;
+    private $prendaModel;
 
     public function __construct() {
-        $this->prenda = new Prenda();
+        $this->prendaModel = new Prenda();
     }
 
-    public function handleRequest($method, $uri, $input) {
-        switch ($method) {
-            case 'GET':
-                if (isset($uri[3])) {
-                    $this->getPrenda($uri[3]);
-                } else {
-                    $this->getPrendas();
-                }
-                break;
-            case 'POST':
-                $this->createPrenda($input);
-                break;
-            case 'PUT':
-                $this->updatePrenda($uri[3], $input);
-                break;
-            case 'DELETE':
-                $this->deletePrenda($uri[3]);
-                break;
-            default:
-                http_response_code(405);
-                echo json_encode(['message' => 'Method not allowed']);
-                break;
+    // Método para obtener los detalles de una prenda por ID
+    public function getPrendaById($id) {
+        $prenda = $this->prendaModel->getById($id);
+        if ($prenda) {
+            // Devolver detalles de la prenda en formato JSON
+            $response = array(
+                'id' => $prenda->getId(),
+                'nombre' => $prenda->getNombre(),
+                'tipo' => $prenda->getTipo(),
+                'precio' => $prenda->getPrecio()
+            );
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode($response);
+        } else {
+            // Prenda no encontrada
+            http_response_code(404); // No encontrado
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode(array('message' => 'Prenda not found'));
         }
-    }
-
-    private function getPrendas() {
-        $result = $this->prenda->read();
-        echo json_encode($result);
-    }
-
-    private function getPrenda($id) {
-        $result = $this->prenda->readOne($id);
-        echo json_encode($result);
-    }
-
-    private function createPrenda($input) {
-        $this->prenda->create($input);
-        echo json_encode(['message' => 'Prenda creada']);
-    }
-
-    private function updatePrenda($id, $input) {
-        $this->prenda->update($id, $input);
-        echo json_encode(['message' => 'Prenda actualizada']);
-    }
-
-    private function deletePrenda($id) {
-        $this->prenda->delete($id);
-        echo json_encode(['message' => 'Prenda eliminada']);
     }
 }
