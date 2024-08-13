8aaad0001f8eba3b9314874ac5ed4bb2952d80d2
NiLJ22 <nicoleloji@gmail.com>
2024-08-12 18:58:50 -0600

    Update VentasController.php

--- API/controllers/VentasController.php
+++ API/controllers/VentasController.php
@@ -1,60 +1,31 @@
 <?php
-require 'models/Venta.php';
+require_once __DIR__ . '/../models/Venta.php';
 
 class VentasController {
-    private $venta;
+    private $ventaModel;
 
     public function __construct() {
-        $this->venta = new Venta();
+        $this->ventaModel = new Venta();
     }
 
-    public function handleRequest($method, $uri, $input) {
-        switch ($method) {
-            case 'GET':
-                if (isset($uri[3])) {
-                    $this->getVenta($uri[3]);
-                } else {
-                    $this->getVentas();
-                }
-                break;
-            case 'POST':
-                $this->createVenta($input);
-                break;
-            case 'PUT':
-                $this->updateVenta($uri[3], $input);
-                break;
-            case 'DELETE':
-                $this->deleteVenta($uri[3]);
-                break;
-            default:
-                http_response_code(405);
-                echo json_encode(['message' => 'Method not allowed']);
-                break;
+    // Método para obtener los detalles de una venta por ID
+    public function getVentaById($id) {
+        $venta = $this->ventaModel->getById($id);
+        if ($venta) {
+            // Devolver detalles de la venta en formato JSON
+            $response = array(
+                'id' => $venta->getId(),
+                'cliente' => $venta->getCliente(),
+                'fecha' => $venta->getFecha(),
+                'total' => $venta->getTotal()
+            );
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode($response);
+        } else {
+            // Venta no encontrada
+            http_response_code(404); // No encontrado
+            header("Content-Type: application/json; charset=UTF-8");
+            return json_encode(array('message' => 'Venta not found'));
         }
-    }
-
-    private function getVentas() {
-        $result = $this->venta->read();
-        echo json_encode($result);
-    }
-
-    private function getVenta($id) {
-        $result = $this->venta->readOne($id);
-        echo json_encode($result);
-    }
-
-    private function createVenta($input) {
-        $this->venta->create($input);
-        echo json_encode(['message' => 'Venta creada']);
-    }
-
-    private function updateVenta($id, $input) {
-        $this->venta->update($id, $input);
-        echo json_encode(['message' => 'Venta actualizada']);
-    }
-
-    private function deleteVenta($id) {
-        $this->venta->delete($id);
-        echo json_encode(['message' => 'Venta eliminada']);
     }
 }
